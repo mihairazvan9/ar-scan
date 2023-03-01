91b3b4e1fad506b5626eee1f9b468bd5c96c0287
Iov Mihai <97.mihairazvan@gmail.com>
2023-03-01 13:25:27 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,68 @@
 <script setup>
-  import patt from './assets/ar/pattern-173941091_835664203961409_5789995235306138562_n (1).patt'
+  // import patt from './assets/ar/pattern-173941091_835664203961409_5789995235306138562_n (1)'
   import blue from './assets/ar/blue.png'
+  import car from './assets/ar/car.glb'
+  import nft from './assets/ar/nft.fset3'
 </script>
 
 <template>
-  <a-scene embedded arjs>
-    <a-marker type='pattern' :url='patt'>
-      <a-image :src="blue"
-               position= "0 1 0"
-               scale= "1 1 1"
-               rotation="0 2 0" animation="property: rotation; to: 0 360 0; loop: true; dur: 7000"
-      />
-    </a-marker>
-<!--    <a-marker type='pattern' url='pattern-174125789_594070895323093_37739543886445794_n.patt'>-->
-<!--      <a-image src="klipartz.com.png"-->
-<!--               position= "0 1 0"-->
-<!--               scale= "1.5 1.5 1.5"-->
-<!--               rotation="3 3 3" animation="property: rotation; to: 0 360 0; loop: true; dur: 7000"-->
-<!--      />-->
-<!--    </a-marker>-->
-<!--    <a-marker type='pattern' url='yellowboots.patt'>-->
-<!--      <a-image src="duck.png"-->
+<!--  <a-scene embedded arjs>-->
+<!--&lt;!&ndash;    <a-marker type='pattern' :url='patt'>&ndash;&gt;-->
+<!--&lt;!&ndash;    <a-assets>&ndash;&gt;-->
+<!--&lt;!&ndash;      <a-asset-item id="CesiumMan" src="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/kalwalt/kalwalt-interactivity-AR/master/resources/models/CesiumMan/glTF/CesiumMan.gltf"></a-asset-item>&ndash;&gt;-->
+<!--&lt;!&ndash;    </a-assets>&ndash;&gt;-->
+<!--    <a-marker preset="hiro">-->
+<!--      <a-image :src="blue"-->
 <!--               position= "0 1 0"-->
 <!--               scale= "1 1 1"-->
-<!--               rotation="3 3 3 " animation="property: rotation; to: 0 360 0; loop: true; dur: 7000"-->
+<!--               rotation="0 2 0" animation="property: rotation; to: 0 360 0; loop: true; dur: 7000"-->
 <!--      />-->
+<!--      <a-entity gltf-model="#CesiumMan"-->
+<!--                animation-mixer-->
+<!--                position="0 0 0"-->
+<!--                scale="1 1 1"-->
+<!--                rotation="0 -90 0"-->
+<!--      ></a-entity>-->
 <!--    </a-marker>-->
-<!--    <a-marker type='pattern' url='pattern-174280781_2056937117782010_3230409652489401640_n.patt'>-->
-<!--      <a-image src="greendino.png"-->
-<!--               position= "0 1 0"-->
-<!--               scale= "1 1 1"-->
-<!--               rotation="3 3 3" animation="property: rotation; to: 0 360 0; loop: true; dur: 7000"-->
-<!--      />-->
-<!--    </a-marker>-->
-<!--    <a-marker type='pattern' url='pattern-174368396_460947292023688_6554398429483939075_n.patt'>-->
-<!--      <a-image src="krab.png"-->
-<!--               position= "0 1 0"-->
-<!--               scale= "1 1 1"-->
-<!--               rotation="2 2 2" animation="property: rotation; to: 0 360 0; loop: true; dur: 7000"-->
-<!--      />-->
-<!--    </a-marker>-->
-<!--    <a-marker type='pattern' url='pattern-174324655_3838406072920098_6987965916776405029_n.patt'>-->
-<!--      <a-image src="butterfly.png"-->
-<!--               position= "0 1 0"-->
-<!--               scale= "1 1 1"-->
-<!--               rotation="3 3 3" animation="property: rotation; to: 0 360 0; loop: true; dur: 7000"-->
-<!--      />-->
-<!--    </a-marker>-->
+<!--    <a-entity camera></a-entity>-->
+<!--  </a-scene>-->
+
+
+  <div class="arjs-loader">
+    <div>Loading...</div>
+  </div>
+  <a-scene
+      vr-mode-ui="enabled: false;"
+      renderer='antialias: true; alpha: true; precision: mediump;'
+      embedded arjs='trackingMethod: best; sourceType: webcam; debugUIEnabled: false;'
+  >
+    <a-assets>
+      <!-- <a-asset-item id="model" src="../resources/sample-entity.glb"></a-asset-item> -->
+      <a-asset-item id="model" :src="car"></a-asset-item>
+      <img id="tex-image" :src="blue">
+    </a-assets>
+
+    <!-- <a-nft
+        samplehandler
+        type='nft' url="../resources/ramen-1000px"
+        smooth="true" smoothCount="10" smoothTolerance="0.01" smoothThreshold="5"
+    > -->
+    <a-nft
+        samplehandler
+        type='nft' :url="nft"
+        smooth="true"
+        smoothCount="10"
+        smoothTolerance="0.01"
+        smoothThreshold="5"
+    >
+      <!-- <a-entity modify-materials gltf-model="#model" material="src:#tex-image" play-all-model-animations="" position="0 0 0" rotation="0 0 0" scale="100 100 100" visible="true">
+      </a-entity> -->
+      <a-entity modify-materials gltf-model="#model" play-all-model-animations="" position="0 500 0" rotation="-90 0 0" scale="50 50 50" visible="true">
+      </a-entity>
+    </a-nft>
     <a-entity camera></a-entity>
   </a-scene>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style lang="scss">
 </style>
--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  assetsInclude: ['**/*.glb', '**/*.patt', '**/*.usdz']
+  assetsInclude: ['**/*.glb', '**/*.patt', '**/*.usdz', '**/*.iset', '**/*.fset3', '**/*.fset']
 })
